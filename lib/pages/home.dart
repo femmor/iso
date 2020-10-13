f593f5987c124bfd19112874bc81d30b0139883c
Emmanuel <fegomson@gmail.com>
2020-10-13 17:57:24 -0500

    added firebase cloud messaging

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,41 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
 import 'package:iso/pages/student_service.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String title = "Title";
+  String helperText = "Helper";
+
+  FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _firebaseMessaging.configure(
+      onMessage: (message) async {
+        setState(() {
+          title = message["notification"]["title"];
+          helperText = "You have received a new notification";
+        });
+      },
+
+      onResume: (message) async {
+        setState(() {
+          title = message["notification"]["title"];
+          helperText = "You have received a new notification";
+        });
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,11 +47,13 @@ class Home extends StatelessWidget {
             colors: [Colors.lightBlue[400], Colors.lightBlue[900]]),
       ),
       body: Container(
-        decoration: BoxDecoration(
-          color: Color(0xFFEEEEEE),
+          decoration: BoxDecoration(
+            color: Color(0xFFEEEEEE),
+          ),
+          child: StudentServices(),
         ),
-        child: StudentServices(),
-      ),
     );
   }
 }
+
+
